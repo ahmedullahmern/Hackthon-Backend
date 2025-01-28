92c5b1728bc09f81b49e988e635902ccbe8ec9ec
ahmedullahmern <ahmedullahmern@gmail.com>
2025-01-28 23:08:09 +0500

    add vercel file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import morgan from 'morgan'
 import 'dotenv/config'
 import cors from 'cors'
 import authRoutes from './routers/auth.js'
-import usersRoutes from './routers/users.js'
+// import usersRoutes from './routers/users.js'
 import mongoose from 'mongoose'
 
 const app = express()
@@ -16,7 +16,7 @@ app.use(morgan("tiny"));
 app.use(express.json())
 
 app.use('/auth', authRoutes)
-app.use('/users', usersRoutes)
+// app.use('/users', usersRoutes)
 
 mongoose.connect(process.env.MONGODBURI).then(() => console.log("mongodb connected"))
     .catch((e) => console.log("error==>", e))
@@ -26,5 +26,6 @@ app.get("/", (req, res) => {
     // console.log("req=>", req.reqByAhmed)
     res.send("Hello Backend")
 })
+
 
 app.listen(PORT, () => console.log("Server Is Running PORT" + PORT))
--- routers/auth.js
+++ routers/auth.js
@@ -1,37 +1,48 @@
 import express from 'express';
 import 'dotenv/config'
-import User from '../models/user.js';
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken';
 import Joi from 'joi';
 import sendResponse from '../helpers/sendResponse.js';
 import { loginSchema, registerSchema } from '../validation.js/authvalidation.js';
+import User from '../models/auth.js';
 const router = express.Router()
 
 
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
 
-router.post("/register", async (req, res) => {
-    const { error, value } = registerSchema.validate(req.body);
-    if (error) return sendResponse(res, 400, null, true, error.message)
-    const user = await User.findOne({ email: value.email })
-    if (user) return sendResponse(res, 403, null, true, "User With This Email already Exist")
-    const hashedPassword = await bcrypt.hash(value.password, 12)
-    value.password = hashedPassword;
-    let newUser = new User({ ...value });
-    newUser = await newUser.save()
-    sendResponse(res, 201, newUser, false, "User Register successfully")
-})
+        // Dummy data for hackathon
+        const users = [
+            { email: "saylani@admin.com", password: "saylani123", role: "admin" },
+            { email: "saylani@receptionist.com", password: "saylani123", role: "receptionist" },
+            { email: "saylani@department.com", password: "saylani123", role: "department" }
+        ];
+
+        // Find user
+        const user = users.find(u => u.email === email && u.password === password);
+
+        if (!user) return res.status(401).send({ message: "Invalid credentials" });
+
+        sendResponse(res, 201, user, false, "User Login SuccessFully");
+    } catch (error) {
+        console.log("errorr=>", error)
+        sendResponse(res, 400, null, false, error.message);
+    }
+});
 
 
-router.post("/login", async (req, res) => {
-    const { error, value } = loginSchema.validate(req.body);
-    if (error) return sendResponse(res, 400, null, true, error.message)
-    const user = await User.findOne({ email: value.email }).lean()
-    if (!user) return sendResponse(res, 403, null, true, "User not Registered.")
-    const isPasswordValid = await bcrypt.compare(value.password, user.password)
-    if (!isPasswordValid) return sendResponse(res, 403, null, true, "Invalid  Credentails")
-    var token = jwt.sign(user, process.env.AUTH_SECRET);
-    sendResponse(res, 200, { user, token }, false, "User Login successfully")
-})
+
+// router.post("/login", async (req, res) => {
+//     const { error, value } = loginSchema.validate(req.body);
+//     if (error) return sendResponse(res, 400, null, true, error.message)
+//     const user = await User.findOne({ email: value.email }).lean()
+//     if (!user) return sendResponse(res, 403, null, true, "User not Registered.")
+//     const isPasswordValid = await bcrypt.compare(value.password, user.password)
+//     if (!isPasswordValid) return sendResponse(res, 403, null, true, "Invalid  Credentails")
+//     var token = jwt.sign(user, process.env.AUTH_SECRET);
+//     sendResponse(res, 200, { user, token }, false, "User Login successfully")
+// })
 
 export default router
